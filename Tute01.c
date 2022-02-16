47dd04dbda6342ac162c442067df8b6da2e2aed6
WOOHI84 <97064107+IT21169830@users.noreply.github.com>
2022-02-16 18:15:08 +0000

    Tutorial 01

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
-  
+  int mark1 , mark2 ,sum ;
+  float average ;
+
+  printf("Enter mark of subject 1:");
+  scanf("%i",&mark1);
+
+  printf("Enter mark of subject 2:");
+  scanf("%i",&mark2);
+
+  sum = mark1 + mark2;
+  average = sum/2;
+
+  printf("Average is:%.1f" ,average);
   return 0;
 }
 
--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,16 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+  int num , i , sum=0;
+
+  printf("Enter number:");
+  scanf("%d",&num);
+
+  for(i=0;i<=num;i++){
+    sum= sum+i;
+   
+  }
+  printf("Sum is:%d",sum);
   return 0;
 }
 
